560c29d0711ce1191603a8b218addd7af349f456
Joseph Nicolas <josephnicolas10@gmail.com>
2022-07-01 14:16:47 -0400

    Added database and token commands

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 import discord
 import os
 import random
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from discord.ext import commands
-
-
 from dotenv import load_dotenv
 
 load_dotenv()
+with open("config.json") as f:
+    config = json.load(f)
+    token = config["TOKEN"]
+    project_id = config["PROJECT_ID"]
 
-token = os.getenv('DISCORD_TOKEN')
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': project_id,
+})
+
+db = firestore.client()
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix='$')
-
-client.command()
-async def test(ctx):
-    pass
+client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
 
+@client.command()
+async def setToken(message, arg):
+    doc_ref = db.collection('users').document(str(message.author.id))
+    doc_ref.set({
+        'tokens': int(arg),
+    })
+    doc = doc_ref.get()
+    await message.send(f"{message.author} has {doc.to_dict()}")
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split('#')[0]
-    user_message = str(message.content)
-    channel = str(message.channel.name)
-    print(f'{username}: {user_message} ({channel})')
-    if message.author == client.user:
-        return
-    if message.channel.name == 'bot-testing':
-        if user_message.lower() == 'hello':
-            await message.channel.send(f'Hello {username}!')
-            return
-        elif user_message.lower() == 'bye':
-            await message.channel.send(f'See you later {username}')
-            return
-        elif user_message.lower() == '!random':
-            response = f'This is your random number: {random.randrange(100000)}'
-            await message.channel.send(response)
-            return
-    if user_message.lower() == '!anywhere':
-        await message.channel.send('This can be used anywhere')
-        return
+@client.command()
+async def addToken(message, arg):
+    doc_ref = db.collection('users').document(str(message.author.id))
+    doc = doc_ref.get()
 
+    await message.send(f"{message.author} had {doc.to_dict()}")
+    tokens = doc_ref.get().get("tokens")
+    doc_ref.set({
+        'tokens': tokens + int(arg),
+    })
+    doc = doc_ref.get()
+    await message.send(f"{message.author} now has {doc.to_dict()}")
+
+@client.command()
+async def viewToken(message):
+    doc_ref = db.collection('users').document(str(message.author.id))
+    doc = doc_ref.get()
+    await message.send(f"{message.author} has {doc.to_dict()}")
+
+    
 client.run(token)
